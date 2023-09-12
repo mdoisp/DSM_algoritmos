d9890c6f833b6ce887a9eb3764145a7f07cc19da
Matheus Prado <ppmatheus01@gmail.com>
2023-09-11 22:36:26 -0300

    Aula 5, exercícios

---
+++ Aula_05/ex_04.py
@@ -0,0 +1,13 @@
+lado_1 = float(input("Digite o primeiro valor: "))
+lado_2 = float(input("Digite o segundo valor: "))
+lado_3 = float(input("Digite o terceiro valor: "))
+mensagem = ""
+
+if : ((x+y > z) and (x+z >y) and (y+z > x)):
+    if (x == y == z):
+    print("É um triângulo Isósceles")
+    elif ((x == y)
+else:
+    mensagem = "Não é possível formar um triângulo"
+
+print(mensagem)
